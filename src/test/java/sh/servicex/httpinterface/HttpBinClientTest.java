2cc8bc02e51adf2beaae3a8a75f84ff15f0ae776
linux_china <libing.chen@gmail.com>
2022-08-12 17:10:54 -0700

    test: HttpBinClientTest

---
+++ src/test/java/sh/servicex/httpinterface/HttpBinClientTest.java
@@ -0,0 +1,18 @@
+package sh.servicex.httpinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+public class HttpBinClientTest {
+    @Test
+    public void testClient() throws Exception {
+        WebClient webClient = WebClient.builder().build();
+        HttpServiceProxyFactory httpServiceProxyFactory = new HttpServiceProxyFactory(WebClientAdapter.forClient(webClient));
+        httpServiceProxyFactory.afterPropertiesSet();
+        HttpBinClient httpBinClient = httpServiceProxyFactory.createClient(HttpBinClient.class);
+        System.out.println(httpBinClient.myIp().origin());
+        System.out.println(httpBinClient.post("Hello").data());
+    }
+}
