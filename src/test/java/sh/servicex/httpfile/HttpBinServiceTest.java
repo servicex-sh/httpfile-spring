016bd25be5e1d1fbfaaf8f479eeaf8d5111ead00
linux_china <libing.chen@gmail.com>
2022-08-12 17:09:51 -0700

    test: HttpBinServiceTest

---
+++ src/test/java/sh/servicex/httpfile/HttpBinServiceTest.java
@@ -0,0 +1,48 @@
+package sh.servicex.httpfile;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import sh.servicex.httpfile.spring.HttpFileProxyFactory;
+
+import java.lang.reflect.Method;
+import java.nio.charset.StandardCharsets;
+
+public class HttpBinServiceTest {
+    private static HttpBinService httpBinService;
+
+    @BeforeAll
+    public static void setUp() {
+        HttpFileProxyFactory httpFileProxyFactory = new HttpFileProxyFactory(WebClientAdapter.forClient(WebClient.builder().build()));
+        httpBinService = httpFileProxyFactory.createClient(HttpBinService.class);
+    }
+
+    @Test
+    public void testHttpGetIp() throws Exception {
+        System.out.println(httpBinService.myIp().origin());
+    }
+
+    @Test
+    public void testHttpPostDemo() throws Exception {
+        System.out.println(httpBinService.post("linux_china").data());
+    }
+
+    @Test
+    public void testRequest() {
+        for (Method method : HttpBinService.class.getMethods()) {
+            final boolean matched = AnnotatedElementUtils.hasAnnotation(method, HttpRequestName.class);
+            if (matched) {
+                System.out.println(method.getName());
+            }
+        }
+    }
+
+    @Test
+    public void testReadHttpFile() throws Exception {
+        final String httpFileText = StreamUtils.copyToString(this.getClass().getResourceAsStream("/httpbin.http"), StandardCharsets.UTF_8);
+        System.out.println(httpFileText);
+    }
+}
